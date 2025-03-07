33d8f6603666e38a9f922b87f1543a139cfb7ddf
phili <0758510@cegepoutaouais.qc.ca>
2025-03-06 19:37:26 -0500

    Ajoutez des fichiers projet.

---
+++ PrepExam2_CodeBehind/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PrepExam2_CodeBehind
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Produit Produit { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            try 
+            {
+                Produit = new Produit
+                {
+                    Nom = txtBoxNom.Text,
+                    Code = int.Parse(txtBoxCode.Text),
+                    Prix = double.Parse(txtBoxPrix.Text),
+                    PrixVente = double.Parse(txtBoxPrixVente.Text),
+                    Quantite = int.Parse(txtBoxQuantite.Text)
+                };
+                txtBlockDisplay.Text = Produit.ToString();
+                Produit.Sauvegarder();
+                lblErreur.Foreground = Brushes.Green;
+                lblErreur.Content = "Sauvegarde réussie";
+                txtBoxNom.Text = "";
+                txtBoxCode.Text = "";
+                txtBoxPrix.Text = "";
+                txtBoxPrixVente.Text = "";
+                txtBoxQuantite.Text = "";
+                txtBoxRechcherCode.Text = "";
+            }
+            catch (Exception ex)
+            {
+                lblErreur.Foreground = Brushes.Red;
+                lblErreur.Content = ex.Message;
+            }
+            
+        }
+
+        private void btnOuvrir_Click(object sender, RoutedEventArgs e)
+        {
+            try 
+            {
+                Produit = Produit.Ouvrir(int.Parse(txtBoxRechcherCode.Text));
+                txtBlockDisplay.Text = Produit.ToString();
+                lblErreur.Foreground = Brushes.Green;
+                lblErreur.Content = "Ouverture réussie";
+                txtBoxNom.Text = Produit.Nom;
+                txtBoxCode.Text = Produit.Code + "";
+                txtBoxPrix.Text = Produit.Prix + "";
+                txtBoxPrixVente.Text = Produit.PrixVente + "";
+                txtBoxQuantite.Text = Produit.Quantite + "";
+            }
+            catch (Exception ex)
+            {
+                lblErreur.Foreground = Brushes.Red;
+                lblErreur.Content = ex.Message +"\nLe produit que vous recherchez n'existe pas.";
+            }
+        }
+    }
+}
---
+++ PrepExam2_CodeBehind/Produit.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+
+namespace PrepExam2_CodeBehind
+{
+    public class Produit
+    {
+        private string _nom;
+        private int _code;
+        private double _prix;
+        private double _prixVente;
+        private int _quantite;
+        public string Nom 
+        {
+            get => _nom;
+            set 
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Le nom ne peut pas être vide");
+                }
+                _nom = value;
+            }
+        }
+        public int Code 
+        {
+            get => _code;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Le code ne peut pas être négatif ou égal à 0");
+                }
+                _code = value;
+            }
+        }
+        public double Prix
+        {
+            get => _prix;
+            set 
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Le prix ne peut pas être négatif.");
+                }
+                _prix = value;
+            }
+        }
+        public double PrixVente
+        {
+            get => _prixVente;
+            set
+            {
+                if (value <= Prix)
+                {
+                    throw new ArgumentException("Le prix de vente ne peut pas être plus bas ou égal au prix stock.");
+                }
+                _prixVente = value;
+            }
+        }
+        public int Quantite
+        {
+            get => _quantite;
+            set 
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("La quantité ne peut pas être négative ou égale à 0");
+                }
+                _quantite = value;
+            }
+        }
+        public double MargeProfit => PrixVente - Prix;
+        public double MontantTotal => PrixVente * Quantite;
+        public Produit()
+        {
+        }
+        public Produit(string nom, int Code, double prix, double prixVente, int quantite)
+        {
+            
+            Nom = nom;
+            Code = Code;
+            Prix = prix;
+            PrixVente = prixVente;
+            Quantite = quantite;
+        }
+        public override string ToString()
+        {
+            return 
+                $"Nom: {Nom}, Code: {Code}\n" +
+                $"Prix: {Prix}$, Prix de vente: {PrixVente}$\n" +
+                $" Quantité: {Quantite}\n" +
+                $" Marge de profit par item: {MargeProfit}$\n" +
+                $" Montant total: {MontantTotal}$";
+        }
+        // Ajouter les fonctions Sauvegarder et Ouvrir
+        public void Sauvegarder()
+        {
+            string path = $"produit{Code}.json";
+            string json = JsonSerializer.Serialize(this);
+            File.WriteAllText(path, json);
+        }
+        public static Produit Ouvrir(int code)
+        {
+            string path = $"produit{code}.json";
+            string json = File.ReadAllText(path);
+            return JsonSerializer.Deserialize<Produit>(json);
+        }
+    }
+}
